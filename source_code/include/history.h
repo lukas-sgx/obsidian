1294311d19c5ee70948156bf5a50d0a80470a8fd
lukas-sgx <lukas.soigneux@epitech.eu>
2026-05-21 10:54:38 +0200

    fix: coding style errors & logical

--- source_code/include/history.h
+++ source_code/include/history.h
@@ -1,14 +1,27 @@
+/*
+** EPITECH PROJECT, 2026
+** ~/epitech/delivery/G-SEC-210-NCE-2-1-obsidian-2/source_code/include
+** File description:
+** history.h
+*/
+
 #ifndef HISTORY_H
-#define HISTORY_H
+    #define HISTORY_H
 
-#define MAX_COMMAND_LENGTH 256
+    #define MAX_COMMAND_LENGTH 256
+    #define HISTORY_FILE ".obsidian_history"
+    #define MAX_HISTORY_SIZE 1000
 
-void history_init(void);
-void history_add(const char *command);
-void history_clear(void);
-void history_free(void);
-void history_show(void);
-const char *history_get(int index);
-int history_count_get(void);
+typedef struct history_s {
+    char **command;
+    int history_count;
+    int is_admin;
+} history_t;
+
+history_t *history_init(void);
+void history_add(const char *command, history_t *history);
+void history_clear(history_t *history);
+void history_free(history_t *history);
+void history_show(char *input, history_t *history);
 
 #endif /* HISTORY_H */
