558a747d3df75caeb6388e9d25e2f64e8b8b6e3a
lukas-sgx <lukas.soigneux@epitech.eu>
2026-05-21 10:54:53 +0200

    fix: coding style errors & logical

--- source_code/include/utils.h
+++ source_code/include/utils.h
@@ -1,2 +1,14 @@
+/*
+** EPITECH PROJECT, 2026
+** ~/epitech/delivery/G-SEC-210-NCE-2-1-obsidian-2/source_code/include
+** File description:
+** utils.h
+*/
+
+#ifndef UTILS_H
+    #define UTILS_H
+
 void write_color(int fd, const char *color, const char *str);
-void load_obsidianrc();
+void load_obsidianrc(void);
+
+#endif /* UTILS_H */
