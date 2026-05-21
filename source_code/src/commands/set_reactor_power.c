9d33818507acb53c10428378d1996a6a788dcf63
lukas-sgx <lukas.soigneux@epitech.eu>
2026-05-21 10:57:30 +0200

    fix: coding style errors & logical

--- source_code/src/commands/set_reactor_power.c
+++ source_code/src/commands/set_reactor_power.c
@@ -9,34 +9,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include "obsidian.h"
 
-void set_reactor_power()
+static void print_overflow_warning(void)
 {
-    char power[11];
+    printf("\nReactor power adjustment may be incorrect.\n");
+    printf("Reactor systems are behaving erratically!\n");
+    printf("Reactor core temperature rising uncontrollably...\n");
+    printf("{12EXPLOSION34}\n");
+    printf("Emergency shutdown initiated!\n\n");
+    exit(1);
+}
 
-    printf("Enter reactor power level: ");
-    fgets(power, sizeof(power), stdin);
-    power[strcspn(power, "\n")] = 0;
-
-    int input_power = atoi(power);
-    int new_power;
-
-    if (input_power > INT_MAX - 1000) { // Int Overflow
-        printf("\nReactor power adjustment may be incorrect.\n");
-        printf("Reactor systems are behaving erratically!\n");
-        printf("Reactor core temperature rising uncontrollably...\n");
-        printf("{12EXPLOSION34}\n");
-        printf("Emergency shutdown initiated!\n\n");
-        exit(1);
-    }
-
-    new_power = input_power + 1000;
-
+static void print_power_result(int new_power)
+{
     if (new_power < 0) {
         printf("Reactor power set to an invalid value: %d\n", new_power);
         printf("Reactor core meltdown imminent!\n");
-    } else {
-        printf("Reactor power set to: %d\n", new_power);
-        printf("Reactor operating within safe parameters.\n");
+        return;
     }
+    printf("Reactor power set to: %d\n", new_power);
+    printf("Reactor operating within safe parameters.\n");
+}
+
+void set_reactor_power(char *input, history_t *history)
+{
+    char power[11] = {0};
+    int input_power = 0;
+    int new_power = 0;
+
+    (void)input;
+    (void)history;
+    printf("Enter reactor power level: ");
+    fgets(power, sizeof(power), stdin);
+    power[strcspn(power, "\n")] = 0;
+    input_power = atoi(power);
+    if (input_power > INT_MAX - 1000)
+        print_overflow_warning();
+    new_power = input_power + 1000;
+    print_power_result(new_power);
 }
